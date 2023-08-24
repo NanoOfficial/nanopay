4d53cfa137c06676a94b3436329180013d1b9efb
krishpranav <krisna.pranav@gmail.com>
2023-08-24 18:56:41 +0530

    score_suffix: extconf

--- ext/score_suffix/extconf.rb
+++ ext/score_suffix/extconf.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 require 'mkmf'
 
 $warnflags = ''
-$CXXFLAGS << '-std=c++11 -Wno-deprecated-register'
 
-create_makefile 'score_suffx/score_suffix'
+$CXXFLAGS << ' -std=c++11 -Wno-deprecated-register'
+$CXXFLAGS.gsub!('-Wimplicit-int', '')
+$CXXFLAGS.gsub!('-Wdeclaration-after-statement', '')
+$CXXFLAGS.gsub!('-Wimplicit-function-declaration', '')
+
+create_makefile 'score_suffix/score_suffix'
